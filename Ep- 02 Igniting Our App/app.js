0213d4a46ffdb819715b887ce58ac674762aa3f6
Esakki Muthu <esakkimuthu6842@gmail.com>
2024-01-12 07:57:17 +0530

    rebasing

---
+++ Ep- 02 Igniting Our App/app.js
@@ -0,0 +1,5 @@
+const h1 = React.createElement("h1",{"id":"heading"},"Hello World from React!!! .... ");
+console.log(h1);
+
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(h1);
