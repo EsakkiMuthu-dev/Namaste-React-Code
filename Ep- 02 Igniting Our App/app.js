0545b371aa20a07c3a0b6d574d6f0c068c08f52d
Esakki Muthu <esakkimuthu6842@gmail.com>
2024-01-13 19:59:13 +0530

    modified git ignore

--- Ep- 02 Igniting Our App/app.js
+++ Ep- 02 Igniting Our App/app.js
@@ -1,6 +1,9 @@
-const h1 = React.createElement("h1",{"id":"heading"},"Hello World from React!!! .... ");
+import React from "react";
+import  ReactDOM  from "react-dom";
+const h1 = React.createElement("h1",{"id":"heading"},"Hello World from React!!🥰! .... ");
 console.log(h1);
 
 
 const root= ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(h1);
