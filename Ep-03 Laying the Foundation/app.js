47e88ee4be38d5f2d50876c9666f0dd1d83e122d
Esakki Muthu <esakkimuthu6842@gmail.com>
2024-01-14 11:35:16 +0530

    Step into Fucntional Components

--- Ep-03 Laying the Foundation/app.js
+++ Ep-03 Laying the Foundation/app.js
@@ -1,6 +1,24 @@
-const a = 10;
+import React from "react";
+import ReactDOM from "react-dom";
 
-function abc() {
-  console.log("hello");
-  console.log("first");
-}
+// React Functional Component
+const HeadingComponent = () => <h1> React Functional Component</h1>;
+const BodyComponent = () => (
+  <>
+    <HeadingComponent />
+    <hr></hr>
+    <h3>Let Write Somethng</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+  </>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BodyComponent />);
